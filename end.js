27d1f4f75d2c537cf8569d7dae7cb3498d7f70a0
JustMax7CB <maximsh4@ac.sce.ac.il>
2022-02-10 22:56:19 +0200

    first commit

---
+++ end.js
@@ -0,0 +1,28 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalscore = document.getElementById('finalscore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
+
+finalscore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+    e.preventDefault();
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+
+    highScores.sort((a, b) => b.score - a.score);
+
+    highScores.splice(MAX_HIGH_SCORES);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign('/');
+}
